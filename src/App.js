c8b1cd428f4ffc0d6001332ff38a961460ce498a
7-hader <hader.seven.com>
2023-07-27 00:44:03 -0500

    feat: logic management complete and delete

--- src/App.js
+++ src/App.js
@@ -31,7 +31,25 @@ function App() {
     }
   );
 
-  console.log('Los usuarios buscan TODOs de ' + searchValue);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => 
+        todo.text === text      
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => 
+        todo.text === text      
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
   
   return (
     <>
@@ -51,6 +69,8 @@ function App() {
             key={todo.text} 
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
