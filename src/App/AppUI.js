1e7c5926aa0c2bce7ac2396a53eec8cac604b280
7-hader <hader.seven.com>
2023-08-08 23:58:57 -0500

    feat: modification form consume context component TODO list

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
@@ -9,45 +10,42 @@ import { EmptyTodos } from '../EmptyTodos';
 import { TodoContext } from '../TodoContext';
 
 function AppUI() {
+  const {
+    loading,
+    error,
+    searchedTodos,
+    completeTodo,
+    deleteTodo
+  } = React.useContext(TodoContext);
     return (
         <>
           <TodoCounter />          
-          <TodoSearch />
+          <TodoSearch />            
+              
+          <TodoList>
+            {loading && 
+              <>
+                <TodosLoading />
+                <TodosLoading />
+                <TodosLoading />
+              </>
+            }
+            {error && <TodosError />}
+            {
+              (!loading && searchedTodos === 0) &&
+              <EmptyTodos />
+            }
 
-            <TodoContext.Consumer>
-              {({
-                loading,
-                error,
-                searchedTodos,
-                completeTodo,
-                deleteTodo
-              }) => (
-                <TodoList>
-                  {loading && 
-                    <>
-                      <TodosLoading />
-                      <TodosLoading />
-                      <TodosLoading />
-                    </>
-                  }
-                  {error && <TodosError />}
-                  {
-                    (!loading && searchedTodos === 0) &&
-                    <EmptyTodos />
-                  }
-
-                  {searchedTodos.map(todo => (
-                    <TodoItem 
-                      key={todo.text} 
-                      text={todo.text}
-                      completed={todo.completed}
-                      onComplete={() => completeTodo(todo.text)}
-                      onDelete={() => deleteTodo(todo.text)}
-                    />
-                  ))}
-                </TodoList>
-              )}
-            </TodoContext.Consumer>
+            {searchedTodos.map(todo => (
+              <TodoItem 
+                key={todo.text} 
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+          </TodoList>                          
     
           <CreateTodoButton/>
         </>
